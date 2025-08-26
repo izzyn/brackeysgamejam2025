9db66bceb7b3eb5f147bed84affb331e87d0c3c4
atomgraze <70081083+atomgraze@users.noreply.github.com>
2025-08-26 10:05:29 -0700

    more merging

---
+++ scripts/player/debugger.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var camera_controls_enabled: bool = true
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		toggle_camera_controls()
+	
+	if event.is_action_pressed("reset_player") and Input.is_key_pressed(KEY_SHIFT):
+		get_tree().reload_current_scene()
+
+func toggle_camera_controls():
+	camera_controls_enabled = !camera_controls_enabled
+	
+	if camera_controls_enabled:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func is_camera_control_enabled() -> bool:
+	return camera_controls_enabled
