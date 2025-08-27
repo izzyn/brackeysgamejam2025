4d50bd15c7b01afbbbd8421f0b6ffd873118b93d
atomgraze <70081083+atomgraze@users.noreply.github.com>
2025-08-27 12:27:53 -0700

    Mid work push
    New particles, collisions, rigidbody props, and gate a gate controller. Its easy to use just reference its gate_open() or gate_close() funcs

---
+++ scripts/objects/gate_controller.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+@onready var animation_player = $AnimationPlayer
+@onready var torch_animation_player = $sconce/TorchAnimationPlayer
+@onready var gate_collision = $StaticBody3D/GateCollision
+@onready var area_3d = $Area3D
+@onready var collision_animation_player = $StaticBody3D/CollisionAnimationPlayer
+
+var is_open: bool = false
+
+func open_gate():
+	if is_open:
+		return
+	is_open = true
+	
+	animation_player.play("gate_open")
+	torch_animation_player.play("ignite")
+	collision_animation_player.play("collision_open")
+	await torch_animation_player.animation_finished
+	torch_animation_player.play("on")
+
+func close_gate():
+	if not is_open:
+		return
+	
+	is_open = false
+	animation_player.play("gate_close")
+	torch_animation_player.play("off")
+	collision_animation_player.play("collision_close")
+
+func toggle_gate():
+	if is_open:
+		close_gate()
+	else:
+		open_gate()
+		
+
+func _on_area_3d_body_entered(body):
+	open_gate()
+
+
+func _on_area_3d_body_exited(body):
+	close_gate()
